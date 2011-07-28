ecc70fc5f1be4a40ec78955a4e03c3c5fd5b1fb9
janMaku <taverne.mike@gmail.com>
2011-07-28 12:36:10 +0000

    -anstatauxigis listo A+B per "Partneroj" kaj aldonis logotipo de vinilkosmo kun ligilo

--- muziko.php
+++ muziko.php
@@ -6,19 +6,23 @@ page_header('Muziko');
 <img src="images/earphones.gif" alt="" title="" class="left_img" />
 <b>Muzaiko ludas la plej mojosan muzikon en Esperantujo!</b> Krom niaj <a href="/programeroj">muzikaj programeroj</a>, ni ankaŭ prezentas muzikon dum niaj aliaj programeroj. Tiu muziko venas de nia <i>muzikolisto</i>, kiun redaktas por vi ĉiusemajne la muziko-teamo de Muzaiko!
 </p>
-<div class="title">La A-Listo</div>
-<?php
-# this doesn't work because the admin files have been removed
-#	$playlist = (read_playlist('default_a'));
-#	foreach ($playlist as $item)
-#		print '<b>'.$item['artist'].'</b>&nbsp; - '.$item['title'].'<br />';
-?>
-<div class="title">La B-Listo</div>
-<?php
-#	$playlist = (read_playlist('default_b'));
-#	foreach ($playlist as $item)
-#		print '<b>'.$item['artist'].'</b>&nbsp; - '.$item['title'].'<br />';
-?>
+<div class="title">Partneroj</div>
+<a target="_blank" href="http://vinilkosmo.com"><img src="images/vinilkosmo.gif"></a>
+
+
+<!-- // <?php
+// this doesn't work because the admin files have been removed
+//	$playlist = (read_playlist('default_a'));
+//	foreach ($playlist as $item)
+//		print '<b>'.$item['artist'].'</b>&nbsp; - '.$item['title'].'<br />';
+//?>
+//<div class="title">La B-Listo</div>
+//<?php
+//	$playlist = (read_playlist('default_b'));
+//	foreach ($playlist as $item)
+//		print '<b>'.$item['artist'].'</b>&nbsp; - '.$item['title'].'<br />';
+//?> -->
+
 <?php
 right();
 right_contents();
