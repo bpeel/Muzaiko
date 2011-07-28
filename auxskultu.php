1e5916879dada266d5020fda85433c0aba1bef15
janMaku <taverne.mike@gmail.com>
2011-07-28 12:36:54 +0100

    aldonis ligilon al radionomy kaj al la gugla grupo

--- auxskultu.php
+++ auxskultu.php
@@ -2,8 +2,13 @@
 include('inc/inc.php');
 page_header('Aŭskultu Muzaikon!');
 ?>
-<p>Pardonu, ĝis nun ne eblas publike doni ligilon al Muzaiko, ĉar ni daŭre prilaboras muzikajn rajtojn ktp. Ni esperas ke vi povos aŭskulti tre tre baldaŭ...</p>
+<p>Muzaiko ne ankoraux estas tute preta (Ni daŭre prilaboras muzikajn rajtojn, ktp), tamen vi jam povas aŭskulti <a href="http://www.radionomy.com/en/radio/muzaikoinfo/listen#">provelsendon ĉe radionomy</a>.
+</p>
+<p>
+Kaj se vi volas partopreni en la projekto aŭ simple komenti, bonvolu aliĝi al nia gugla grupo: <a href="https://groups.google.com/group/retradio-projekto">retradio-projekto</a>.
+</p>
 <?php
 right();
 right_contents();
 page_footer();
+
