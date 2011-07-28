cda7425e67a1a1b6807128e16c42ee04a62c3efc
janMaku <taverne.mike@gmail.com>
2011-07-28 16:47:44 +0000

    Aldonis pli da enhavo en programeroj.

--- programeroj.php
+++ programeroj.php
@@ -1,16 +1,28 @@
 <?php
 include('inc/inc.php');
-page_header('Programeroj');
+page_header('');
 ?>
+
+<div class="title">Novaĵoj</div>
+Novaĵoj el la tuta mondo por la tuta mondo, kiun redaktas por vi nia teamo ĉe <a target="_blank" href="https://groups.google.com/group/retradio-novajxoj">Retradio-novaĵoj</a>.
+<!--<a target="_blank" href="https://docs.google.com/document/d/1tEKmKe_OTNb_ZAZ1Dm3eVEXJ3K9sNeSy_yMPbwygCWY/edit?hl=en&authkey=CK7Hq6cH">Kiel verki radio-novaĵojn</a>
+-->
+
+<div class="title">Babilaj programeroj</div>
+<ul>
+<li>Esperantistoj aktivas</li>
+<li>Fabelo</li>
+<li>Esperantistaj muzikistoj</li>
+<li>Raportoj el eventoj</li>
+</ul>
+
+<div class="title">Reklamoj</div>
 
 <div class="title">Muziko</div>
 <p>
 <img src="images/earphones.gif" alt="" title="" class="left_img" />
 <b>Muzaiko ludas la plej mojosan muzikon en Esperantujo!</b> Krom niaj muzikaj programeroj, ni ankaŭ prezentas muzikon dum niaj aliaj programeroj. Tiu muziko venas de nia <i>muzikolisto</i>, kiun redaktas por vi ĉiusemajne la muziko-teamo de Muzaiko!
 </p>
-
-<div class="title">Aliaj programeroj</div>
-<p>Baldaŭ aperos...</p>
 
 <div class="title">Partneroj</div>
 <a target="_blank" href="http://vinilkosmo.com"><img src="images/vinilkosmo.gif"></a>
