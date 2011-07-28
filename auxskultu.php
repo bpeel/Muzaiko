5bc16778c974be0a6140a42592a3069538be7992
janMaku <taverne.mike@gmail.com>
2011-07-28 12:57:51 +0000

    -faris ligilojn malfermi novan pagxon (target="_blank")

--- auxskultu.php
+++ auxskultu.php
@@ -2,10 +2,10 @@
 include('inc/inc.php');
 page_header('Aŭskultu Muzaikon!');
 ?>
-<p>Muzaiko ne ankoraux estas tute preta (Ni daŭre prilaboras muzikajn rajtojn, ktp), tamen vi jam povas aŭskulti <a href="http://www.radionomy.com/en/radio/muzaikoinfo/listen#">provelsendon ĉe radionomy</a>.
+<p>Muzaiko ne ankoraux estas tute preta (Ni daŭre prilaboras muzikajn rajtojn, ktp), tamen vi jam povas aŭskulti <a target="_blank" href="http://www.radionomy.com/en/radio/muzaikoinfo/listen#">provelsendon ĉe radionomy</a>.
 </p>
 <p>
-Kaj se vi volas partopreni en la projekto aŭ simple komenti, bonvolu aliĝi al nia gugla grupo: <a href="https://groups.google.com/group/retradio-projekto">retradio-projekto</a>.
+Kaj se vi volas partopreni en la projekto aŭ simple komenti, bonvolu aliĝi al nia gugla grupo: <a target="_blank" href="https://groups.google.com/group/retradio-projekto">retradio-projekto</a>.
 </p>
 <?php
 right();
