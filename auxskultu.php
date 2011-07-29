c07f62290b96bc83418a6c69c27447ebff3d90aa
Gérald Gounot <gerald@gounot.eu>
2011-07-29 21:36:00 +0200

    Korektas ankoraux ĉapelo

--- auxskultu.php
+++ auxskultu.php
@@ -2,7 +2,7 @@
 include('inc/inc.php');
 page_header('Aŭskultu Muzaikon!');
 ?>
-<p>Muzaiko ne ankoraux estas tute preta (Ni daŭre prilaboras muzikajn rajtojn, ktp), tamen vi jam povas aŭskulti <a target="_blank" href="http://www.radionomy.com/en/radio/muzaikoinfo/listen#">provelsendon ĉe radionomy</a>.
+<p>Muzaiko ne ankoraŭ estas tute preta (Ni daŭre prilaboras muzikajn rajtojn, ktp), tamen vi jam povas aŭskulti <a target="_blank" href="http://www.radionomy.com/en/radio/muzaikoinfo/listen#">provelsendon ĉe radionomy</a>.
 </p>
 <p>
 Kaj se vi volas partopreni en la projekto aŭ simple komenti, bonvolu aliĝi al nia gugla grupo: <a target="_blank" href="https://groups.google.com/group/retradio-projekto">retradio-projekto</a>.
