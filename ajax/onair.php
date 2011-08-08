1bcfdc6c73f5c1f53b710e0c338289d22a528e78
Gérald Gounot <gerald@gounot.eu>
2011-08-08 17:19:14 +0200

    Plibonigas la nun-ludita trajto

--- ajax/onair.php
+++ ajax/onair.php
@@ -5,14 +5,17 @@
 <script type="text/javascript" src="ajax/jquery-1.6.2.min.js"></script>
 <script type="text/javascript">
 function updateCurrentSong(){
-	$('#onair').load('ajax/current_song.php');
+	$('#kanto').load('ajax/current_song.php');
 }
 setInterval("updateCurrentSong()", 5000);
 </script>
 <div id="now-playing">
 <h1>Nun estas ludita…</h1>
-<div><?php include('ajax/current_song.php'); ?></div>
+<div id="kanto"></div>
 </div>
+<script type="text/javascript">
+updateCurrentSong();
+</script>
 
 <!--<p class="showtitle"><?=$show_data['title']?></p>
 <p class="nowplaying"><?=current_song()?></p>
