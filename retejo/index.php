874989f98b9d87d27a6eba5f5585f1d7c538a30f
janMaku <taverne.mike@gmail.com>
2011-09-03 04:26:23 +0100

    nova programo

--- retejo/index.php
+++ retejo/index.php
@@ -10,11 +10,11 @@ page_header('Bonvenon al Muzaiko!', '');
 <div class="title">La hodiaŭa programo</div>
 
 <div>
-  La hodiaŭa elsendo (02/09/2011):
+  03/09/2011:
   <ul>
-    <li>00:00-01:00 UTC: <i>Esperantistoj aktivas</i> prezentas Céline Bernard, kiu rakontos al ni pri la grupo en la urbo de Orange, en Francio</li>
-    <li>01:00-02:00 UTC: <i>TEJO Tutmonde</i> prezentas intervjuon de Dan Mrázek kun la novelektita prezidento de TEJO, Łukasz Żebrowski</li>
-    <li>02:00-03:00 UTC: En la kadro de la programo <i>Bibliotekoj tra la mondo</i>, radio Muzaiko vizitas la Britan Bibliotekon</li>
+    <li>00:00-01:00: <i>Gramatikaĵo</i>; La programo de <i>Esperantistaj muzikistoj</i> prezentas intervjuon kun ĴomArt kaj Nataŝa</li>
+    <li>01:00-02:00: <i>Raportoj el eventoj</i> prezentas IREM, JuSKA kaj Café Esperanto en Parizo</li>
+    <li>02:00-03:00: <i>Esperantistoj aktivas</i> prezentas intervjuon kun Chuck Smith pri blogado ĉe <a target="_blank" href="http://www.transparent.com/esperanto/">transparent.com/esperanto</a>; <a target="_blank" href="http://eo.wikipedia.org/wiki/Julia_Noe">Julia Noe</a> laŭtlegas fabelon "La fabelo kaj la vero"</li>
   </ul>
   kaj poste tiuj tri horoj ripetiĝos dum la tuta tago.
 </div>
@@ -26,17 +26,8 @@ page_header('Bonvenon al Muzaiko!', '');
 <div>
   <ul>
 
-    <li>01/09/2011:
-      <ul>
-	<li>00:00-01:00 UTC: <i>Raportoj el eventoj</i> prezentas raportojn pri IJK, JuSKA kaj TAKE</li>
-	<li>01:00-02:00 UTC: <i>Saluton, Radiemuloj!</i> prezentas la Polan retradion en Esperanto</li>
-	<li>02:00-03:00 UTC: <i>Universitataj sistemoj tra la mondo</i> prezentas la valonan sistemon en Belgio</li>
-      </ul>
-      kaj poste tiuj tri horoj ripetiĝis dum la tuta tago.
-    </li>
-    <br>
-
-    <li>17/08/2011-01/09/2011: 5a provelsendo:
+    <li>
+      02/09/2011:
       <ul>
 	<li>00:00-01:00 UTC: <i>Esperantistoj aktivas</i> prezentas Céline Bernard, kiu rakontos al ni pri la grupo en la urbo de Orange, en Francio</li>
 	<li>01:00-02:00 UTC: <i>TEJO Tutmonde</i> prezentas intervjuon de Dan Mrázek kun la novelektita prezidento de TEJO, Łukasz Żebrowski</li>
@@ -46,7 +37,30 @@ page_header('Bonvenon al Muzaiko!', '');
     </li>
     <br>
 
-    <li>16/08/2011: 4a provelsendo:
+    <li>
+      01/09/2011:
+      <ul>
+	<li>00:00-01:00 UTC: <i>Raportoj el eventoj</i> prezentas raportojn pri IJK, JuSKA kaj TAKE</li>
+	<li>01:00-02:00 UTC: <i>Saluton, Radiemuloj!</i> prezentas la Polan retradion en Esperanto</li>
+	<li>02:00-03:00 UTC: <i>Universitataj sistemoj tra la mondo</i> prezentas la valonan sistemon en Belgio</li>
+      </ul>
+      kaj poste tiuj tri horoj ripetiĝis dum la tuta tago.
+    </li>
+    <br>
+
+    <li>
+      17/08/2011-01/09/2011: 5a provelsendo:
+      <ul>
+	<li>00:00-01:00 UTC: <i>Esperantistoj aktivas</i> prezentas Céline Bernard, kiu rakontos al ni pri la grupo en la urbo de Orange, en Francio</li>
+	<li>01:00-02:00 UTC: <i>TEJO Tutmonde</i> prezentas intervjuon de Dan Mrázek kun la novelektita prezidento de TEJO, Łukasz Żebrowski</li>
+	<li>02:00-03:00 UTC: En la kadro de la programo <i>Bibliotekoj tra la mondo</i>, radio Muzaiko vizitas la Britan Bibliotekon</li>
+      </ul>
+      kaj poste tiuj tri horoj ripetiĝis dum la tuta tago.
+    </li>
+    <br>
+
+    <li>
+      16/08/2011: 4a provelsendo:
       <ul>
 	<li>00:00-01:00 UTC: <i>Raportoj el eventoj</i> prezentas raportojn pri IJK, JuSKA kaj TAKE</li>
 	<li>01:00-02:00 UTC: <i>TEJO Tutmonde</i> prezentas intervjuon de Dan Mrázek kun la novelektita prezidento de TEJO, Łukasz Żebrowski</li>
@@ -56,7 +70,8 @@ page_header('Bonvenon al Muzaiko!', '');
     </li>
     <br>
 
-    <li>07/08/2011 - 15/08/2011: 3a provelsendo:
+    <li>
+      07/08/2011 - 15/08/2011: 3a provelsendo:
       <ul>
 	<li>00:00-01:00 UTC: <i>Raportoj el eventoj</i> prezentas raportojn pri IJK, JuSKA kaj TAKE</li>
 	<li>01:00-02:00 UTC: <i>Saluton, Radiemuloj!</i> prezentas la Polan retradion en Esperanto</li>
