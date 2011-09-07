f9b767491a41836e6e7164010693b1170c49eebd
Gérald Gounot <gerald@gounot.eu>
2011-09-07 19:42:50 +0200

    Indikas charset=utf-8 en HTTP-rubriko por current_song.php

--- retejo/ajax/current_song.php
+++ retejo/ajax/current_song.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-Type: text/html;charset=utf-8');
 $url = 'http://api.radionomy.com/currentsong.cfm?radiouid=14694a7d-9023-4db1-86b4-d85d96cba181&type=xml';
 $xml = simplexml_load_file($url);
 
