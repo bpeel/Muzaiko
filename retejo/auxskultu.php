0092d67554caa9777c62fc5da2f83279b9af3565
Gérald Gounot <gerald@gounot.eu>
2011-09-09 10:47:46 +0200

    Aldonas averton pri la reklamoj de Radionomy

--- retejo/auxskultu.php
+++ retejo/auxskultu.php
@@ -2,6 +2,12 @@
 include('inc/inc.php');
 page_header('Aŭskultu Muzaikon!');
 ?>
+<div style="border: solid red 1px; background-color: #FCC; padding: 5px;">
+<strong>Averto!</strong> Dum la
+provelsenda periodo la teamo de Muzaiko elektis uzi la servojn de
+Radionomy. Tio bedaŭrinde signifas ke oni devas akcepti reklamojn,
+kiuj estas enmetitaj de Radionomy.
+</div>
 <p>Muzaiko ne ankoraŭ estas tute preta (Ni daŭre prilaboras muzikajn rajtojn, ktp), tamen vi jam povas aŭskulti <a target="_blank" href="http://www.radionomy.com/en/radio/muzaikoinfo/listen#">provelsendon ĉe radionomy</a>.
 </p>
 <p>
