48797173f2084ace78817eefb7ffe034323c4289
janMaku <taverne.mike@gmail.com>
2011-09-17 10:17:55 +0100

    dua averto

--- retejo/auxskultu.php
+++ retejo/auxskultu.php
@@ -7,7 +7,7 @@ page_header('Aŭskultu Muzaikon!');
 Dum la provelsenda periodo la teamo de Muzaiko elektis uzi la servojn de Radionomy. Tio bedaŭrinde signifas ke oni devas akcepti reklamojn, kiuj estas enmetitaj de Radionomy.
 </div>
 
-<div style="border: solid red 1px; background-color: #FCC; padding: 5px;">
+<div style="border: solid red 1px; background-color: #FCC; padding: 5px; margin: 5px 0 0 0">
 <strong>Averto!</strong>
 Ni pardonpetas, ke nun ne videblas la kanttitoloj, nek la nomo de la muzikgrupoj. Ni ĝis nun ne komprenas, kial tio okazis, eĉ la forumoj de Radionomy ne funkcias por ke oni povu demandi ilin pri la detaloj. Ĝis ni solvos la problemon, ni petas vian paciencon, kaj por scii, kio estas elsendita, uzu la informojn, kiuj aperas rekte ĉe la elsendo.
 </div>
