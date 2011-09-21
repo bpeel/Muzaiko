cab2ea28f8f1b31827978d0104387af1746d27a1
Gérald Gounot <gerald@gounot.eu>
2011-09-21 13:08:21 +0200

    Forigas averton pri la kantinformoj

--- retejo/auxskultu.php
+++ retejo/auxskultu.php
@@ -7,11 +7,6 @@ page_header('Aŭskultu Muzaikon!');
 Dum la provelsenda periodo la teamo de Muzaiko elektis uzi la servojn de Radionomy. Tio bedaŭrinde signifas ke oni devas akcepti reklamojn, kiuj estas enmetitaj de Radionomy.
 </div>
 
-<div style="border: solid red 1px; background-color: #FCC; padding: 5px; margin: 5px 0 0 0">
-<strong>Averto!</strong>
-Ni pardonpetas, ke nun ne videblas la kanttitoloj, nek la nomo de la muzikgrupoj. Ni ĝis nun ne komprenas, kial tio okazis, eĉ la forumoj de Radionomy ne funkcias por ke oni povu demandi ilin pri la detaloj. Ĝis ni solvos la problemon, ni petas vian paciencon, kaj por scii, kio estas elsendita, uzu la informojn, kiuj aperas rekte ĉe la elsendo.
-</div>
-
 <p>Muzaiko ne ankoraŭ estas tute preta (Ni daŭre prilaboras muzikajn rajtojn, ktp), tamen vi jam povas aŭskulti <a target="_blank" href="http://www.radionomy.com/en/radio/muzaikoinfo/listen#">provelsendon ĉe radionomy</a> aux rekte kun <a href="http://listen.radionomy.com/muzaikoinfo.m3u">via preferata muzikludilo</a>.
 </p>
 <p>
