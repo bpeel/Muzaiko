c100dde00838d42bf97abe6e8769c1bcc88f26a3
janMaku <taverne.mike@gmail.com>
2011-10-02 14:27:21 +0100

    aldonis ligilon al la vikia pagxo pri kiel auxskulti

--- retejo/auxskultu.php
+++ retejo/auxskultu.php
@@ -7,8 +7,14 @@ page_header('Aŭskultu Muzaikon!');
 Dum la provelsenda periodo la teamo de Muzaiko elektis uzi la servojn de Radionomy. Tio bedaŭrinde signifas ke oni devas akcepti reklamojn, kiuj estas enmetitaj de Radionomy.
 </div>
 
-<p>Muzaiko ne ankoraŭ estas tute preta (Ni daŭre prilaboras muzikajn rajtojn, ktp), tamen vi jam povas aŭskulti <a target="_blank" href="http://www.radionomy.com/en/radio/muzaikoinfo/listen#">provelsendon ĉe radionomy</a> aux rekte kun <a href="http://listen.radionomy.com/muzaikoinfo.m3u">via preferata muzikludilo</a>.
+<p>
+Muzaiko ne ankoraŭ estas tute preta (Ni daŭre prilaboras muzikajn rajtojn, ktp), tamen vi jam povas aŭskulti <a target="_blank" href="http://www.radionomy.com/en/radio/muzaikoinfo/listen#">provelsendon ĉe radionomy</a> aux rekte kun <a href="http://listen.radionomy.com/muzaikoinfo.m3u">via preferata muzikludilo</a>.
 </p>
+
+<p>
+Jen pli da informoj pri <a href="https://vikio.muzaiko.info/index.php/Kiel_a%C5%ADskulti_radio_Muzaiko" target="_blank">kiel auxskulti Radio Muzaiko</a>.
+</p>
+
 <p>
 Kaj se vi volas partopreni en la projekto aŭ simple komenti, bonvolu aliĝi al nia gugla grupo: <a target="_blank" href="https://groups.google.com/group/retradio-projekto">retradio-projekto</a>.
 </p>
