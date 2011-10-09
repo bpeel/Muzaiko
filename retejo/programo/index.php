94a6d0e5326236b32b4df07f0f45ff4bf709edc3
Gérald Gounot <gerald@gounot.eu>
2011-10-09 21:19:19 +0300

    Alfabete ordigas la programerojn en la administrilo

--- retejo/programo/index.php
+++ retejo/programo/index.php
@@ -236,7 +236,7 @@
 
 <?php
 
-$query = "SELECT id, description FROM programero";
+$query = "SELECT id, description FROM programero ORDER BY description ASC";
 $result = mysql_query($query);
 while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
 	printf('<tr><td>%s</td><td class="date_column"><ul>', format_programero(htmlspecialchars(stripslashes($row[1]))));
