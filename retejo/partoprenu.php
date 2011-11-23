67bef6286bcef722c992063f99458e1d804a0bea
Gérald Gounot <gerald@gounot.eu>
2011-11-23 11:07:19 +0100

    Aldonu formularon por proponi novaĵojn

--- retejo/partoprenu.php
+++ retejo/partoprenu.php
@@ -32,6 +32,37 @@ pli da informoj.
  --> 	</ul>
 </p>
 
+<h1 id="verku-novajxojn">Verku novaĵojn</h1>
+
+<div id="verku-novajxon-content">
+	<p>Se vi emas, vi povas verki novaĵojn, kiuj eble estos laŭtlegata de Muzaiko. La reguloj por ĝuste verki novaĵojn troviĝas <a href="http://vikio.muzaiko.info/index.php/Novaĵo">ĉe la vikio</a>.</p> 
+	<p>Pensu ke la novaĵoj devu esti koncizaj, facilkompreneblaj, kaj laŭeble neŭtralaj. Ĉiam menciu la fonton, bonvolu!</p>
+
+  <?php
+  	if (!empty($_GET['eraro'])) {
+		if ($_GET['eraro'] == 1)
+			echo '<div class="eraro">Bonvolu plenigi ĉiujn kampojn.</div>';
+		else if ($_GET['eraro'] == 2)
+			echo '<div class="eraro">Via retpoŝtadreso kaj sia konfirmo ne estas same.</div>';
+	}
+  ?>
+
+  <form method="post" action="http://muzaiko.info/sendinovajxon">
+    <fieldset>
+      <legend>Sendi novaĵon</legend>
+      <ol>
+        <li><label for="nomo">Via nomo</label><input type="text" name="nomo" id="nomo"<?php echo (empty($_GET['nomo']) ? '' : ' value="'.htmlspecialchars($_GET['nomo']).'"') ?> /></li>
+        <li><label for="retposxtadreso">Via retpoŝtadreso</label><input type="text" name="retposxtadreso" id="retposxtadreso"<?php echo (empty($_GET['retposxtadreso']) ? '' : ' value="'.htmlspecialchars($_GET['retposxtadreso']).'"') ?> /></li>
+        <li><label for="retposxtadresokonfirmo">Retajpu vian retpoŝtadreson</label><input type="text" name="retposxtadresokonfirmo" id="retposxtadresokonfirmo"<?php echo (empty($_GET['retposxtadresokonfirmo']) ? '' : ' value="'.htmlspecialchars($_GET['retposxtadresokonfirmo']).'"') ?> /></li>
+        <li><label for="titolo">Titolo de la novaĵo</label><input type="text" name="titolo" id="titolo"<?php echo (empty($_GET['titolo']) ? '' : ' value="'.htmlspecialchars($_GET['titolo']).'"') ?> /></li>
+        <li><label for="enhavo">Enhavo de la novaĵo</label><textarea cols="50" rows="20" name="enhavo" id="enhavo"><?php echo (empty($_GET['enhavo']) ? '' : htmlspecialchars($_GET['enhavo'])) ?></textarea></li>
+        <li id="kontrauxspamo"><label for="kontrauxspamo">Ne plenigu tiun kampon</label><input type="text" name="kontrauxspamo" id="kontrauxspamo" /></li>
+        <li><input type="submit" name="novajxo" value="Sendi" /></li>
+      </ol>
+    </fieldset>
+  </form>
+</div>
+
 <?php
 right();
 right_contents();
---
+++ retejo/sendinovajxon.php
@@ -0,0 +1,25 @@
+<?php
+include('inc/inc.php');
+
+include_once '/var/muzaiko/novajxalisto.php';
+
+if (empty($_POST['kontrauxspamo'])) {
+	if (empty($_POST['nomo']) || empty($_POST['retposxtadreso']) || empty($_POST['retposxtadresokonfirmo']) || empty($_POST['titolo']) || empty($_POST['enhavo'])) {
+		header('Location: http://localhost/retejo/partoprenu?eraro=1&nomo='.urlencode($_POST['nomo']).'&retposxtadreso='.urlencode($_POST['retposxtadreso']).'&retposxtadresokonfirmo='.urlencode($_POST['retposxtadresokonfirmo']).'&titolo='.urlencode($_POST['titolo']).'&enhavo='.urlencode($_POST['enhavo']).'#verku-novajxojn');
+		exit();
+	}
+	if ($_POST['retposxtadreso'] != $_POST['retposxtadresokonfirmo']) {
+		header('Location: http://localhost/retejo/partoprenu?eraro=2&nomo='.urlencode($_POST['nomo']).'&retposxtadreso='.urlencode($_POST['retposxtadreso']).'&retposxtadresokonfirmo='.urlencode($_POST['retposxtadresokonfirmo']).'&titolo='.urlencode($_POST['titolo']).'&enhavo='.urlencode($_POST['enhavo']).'#verku-novajxojn');
+		exit();
+	}
+	$ret = mail($recipient, 'Proponita novaĵo: '.$_POST['titolo'], 'Tiu ĉi novaĵo estas proponita de '.$_POST['nomo'].' <'.$_POST['retposxtadreso'].">.\n\n".$_POST['enhavo'], 'From: '.$_POST['nomo'].' <'.$_POST['retposxtadreso'].">\r\nContent-type: text/plain; charset=utf-8");
+	page_header('Sendi novaĵon');
+	if ($ret)
+		echo '<div class="sukceso"><p>Via novaĵo estis sukcese sendita. Vi ricevos respondon baldaŭ. Dankon!</p><p>Eble vi emas <a href="./partoprenu#verku-novajxojn">verki plian novaĵon</a>? :-)</p></div>';
+	else
+		echo '<div class="eraro">Via novaĵo ne estis sendita pro teknika problemo. Provu sendi ĝin retpoŝtmesaĝe al '.$recipient.'. Dankon!</div><div><h1>'.$_POST['titolo'].'</h1>'.$_POST['enhavo'].'</div>';
+}
+
+right();
+right_contents();
+page_footer();
