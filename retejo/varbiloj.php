bb2f77fddc8c03a3c2041caff54caf93a357ecb7
Gérald Gounot <gerald@gounot.eu>
2011-11-27 19:17:45 +0100

    Aldonu varbiloj

---
+++ retejo/varbiloj.php
@@ -0,0 +1,62 @@
+<?php
+include('inc/inc.php');
+page_header('Varbiloj');
+?>
+<ul id="reklambendoj">
+  <li class="reklambendo">
+    <div class="reklambendo-rezultato">
+      <a href="http://www.muzaiko.info/" title="Mojoseco kaj Muziko: Muzaiko!"><img src="reklambendoj/half banner 234x60.jpg" alt="Muzaiko - tuttempa tutesperanta retradio" style="border:0" /></a>
+    </div>
+    <div class="reklambendo-kodo-html">
+      HTML:<br />
+      <textarea>&lt;a href="http://www.muzaiko.info/" title="Mojoseco kaj Muziko: Muzaiko!"&gt;&lt;img src="http://www.muzaiko.info/reklambendoj/half banner 234x60.jpg" alt="Muzaiko - tuttempa tutesperanta retradio" style="border:0" /&gt;&lt;/a&gt;</textarea>
+    </div>
+    <div class="reklambendo-kodo-html">
+      BBCode:<br />
+      <textarea>[url=http://www.muzaiko.info/][img]http://www.muzaiko.info/reklambendoj/half banner 234x60.jpg[/img][/url]</textarea>
+    </div>
+  </li>
+  <li class="reklambendo">
+    <div class="reklambendo-rezultato">
+      <a href="http://www.muzaiko.info/" title="Mojoseco kaj Muziko: Muzaiko!"><img src="reklambendoj/half banner 234x60_1.jpg" alt="Muzaiko - tuttempa tutesperanta retradio" style="border:0" /></a>
+    </div>
+    <div class="reklambendo-kodo-html">
+      HTML:<br />
+      <textarea>&lt;a href="http://www.muzaiko.info/" title="Mojoseco kaj Muziko: Muzaiko!"&gt;&lt;img src="http://www.muzaiko.info/reklambendoj/half banner 234x60_1.jpg" alt="Muzaiko - tuttempa tutesperanta retradio" style="border:0" /&gt;&lt;/a&gt;</textarea>
+    </div>
+    <div class="reklambendo-kodo-html">
+      BBCode:<br />
+      <textarea>[url=http://www.muzaiko.info/][img]http://www.muzaiko.info/reklambendoj/half banner 234x60_1.jpg[/img][/url]</textarea>
+    </div>
+  </li>
+  <li class="reklambendo">
+    <div class="reklambendo-rezultato">
+      <a href="http://www.muzaiko.info/" title="Mojoseco kaj Muziko: Muzaiko!"><img src="reklambendoj/square 240x240.jpg" alt="Muzaiko - tuttempa tutesperanta retradio" style="border:0" /></a>
+    </div>
+    <div class="reklambendo-kodo-html">
+      HTML:<br />
+      <textarea>&lt;a href="http://www.muzaiko.info/" title="Mojoseco kaj Muziko: Muzaiko!"&gt;&lt;img src="http://www.muzaiko.info/reklambendoj/square 240x240.jpg" alt="Muzaiko - tuttempa tutesperanta retradio" style="border:0" /&gt;&lt;/a&gt;</textarea>
+    </div>
+    <div class="reklambendo-kodo-html">
+      BBCode:<br />
+      <textarea>[url=http://www.muzaiko.info/][img]http://www.muzaiko.info/reklambendoj/square 240x240.jpg[/img][/url]</textarea>
+    </div>
+  </li>
+  <li class="reklambendo">
+    <div class="reklambendo-rezultato">
+      <a href="http://www.muzaiko.info/" title="Mojoseco kaj Muziko: Muzaiko!"><img src="reklambendoj/Vertical rectangle 240x400.jpg" alt="Muzaiko - tuttempa tutesperanta retradio" style="border:0" /></a>
+    </div>
+    <div class="reklambendo-kodo-html">
+      HTML:<br />
+      <textarea>&lt;a href="http://www.muzaiko.info/" title="Mojoseco kaj Muziko: Muzaiko!"&gt;&lt;img src="http://www.muzaiko.info/reklambendoj/Vertical rectangle 240x400.jpg" alt="Muzaiko - tuttempa tutesperanta retradio" style="border:0" /&gt;&lt;/a&gt;</textarea>
+    </div>
+    <div class="reklambendo-kodo-html">
+      BBCode:<br />
+      <textarea>[url=http://www.muzaiko.info/][img]http://www.muzaiko.info/reklambendoj/Vertical rectangle 240x400.jpg[/img][/url]</textarea>
+    </div>
+  </li>
+</ul>
+<?php
+right();
+right_contents();
+page_footer();
