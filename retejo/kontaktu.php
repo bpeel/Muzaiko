b06fff0e1984aae0656ce5098b71cb890b7d1bd3
janMaku <taverne.mike@gmail.com>
2011-12-08 02:28:59 +0000

    vizagxlibro+twitter

--- retejo/kontaktu.php
+++ retejo/kontaktu.php
@@ -18,6 +18,12 @@ Vi povas ankaŭ babili kun ni tujmesaĝe:
 	<li><a href="http://webchat.freenode.net?channels=Muzaiko&uio=d4" target="_blank">Per TTT IRC kliento</a> (tajpu kromnomon en la "Nickname" kampo, solvu la CAPTCHA kaj klaku sur "Connect")</li>
 </ul>
 
+<h1>Sociaj retoj</h1>
+<ul>
+	<li><a href="http://eo-eo.facebook.com/pages/Muzaiko/205933802764796?_fb_noscript=1" target="_blank" name="Muzaiko ĉe vizaĝlibro">Muzaiko ĉe vizaĝlibro</a></li>
+	<li><a href="https://twitter.com/Muzaiko" target="_blank" name="Muzaiko ĉe Twitter">Muzaiko ĉe Twitter</a></li>
+<ul>
+
 <?php
 right();
 right_contents();
