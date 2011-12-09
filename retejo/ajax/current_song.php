afc0a58d478cc207212dc14fd997258f4dbaebf5
janMaku <taverne.mike@gmail.com>
2011-12-09 13:34:10 +0000

    aldonante ligilojn al muziktekstoj kaj retpagxoj

--- retejo/ajax/current_song.php
+++ retejo/ajax/current_song.php
@@ -59,85 +59,204 @@ if (@file_exists($cache) && $date > $expire && file_get_contents($cache) != "") 
 
 if($xml ===  FALSE)
 {
-	$teksto = "Neebla ricevi la kantinformojn el Radionomy. :(";
-   echo wordwrap ( $teksto ,  $maxlen , "<br>" , true);
+  $teksto = "Neebla ricevi la kantinformojn el Radionomy. :(";
+  echo wordwrap ( $teksto ,  $maxlen , "<br>" , true);
 }
-else {
-	if (empty($xml->track->artists)) {
-	  $teksto = cxapeligu($xml->track->title);
-     echo wordwrap ( $teksto ,  $maxlen , "<br>" , true);
-	}
-	else {
-	  if (!empty($xml->track->artists) && !empty($xml->track->title)) {
-	    $teksto = cxapeligu($xml->track->artists . " - " . $xml->track->title);
-       echo wordwrap ( $teksto ,  $maxlen , "<br>" , true);
-	
-	    $artists = trim(addslashes(malcxapeligu($xml->track->artists)));
-	    $title = trim(addslashes(malcxapeligu($xml->track->title)));
-	
-	    mysql_connect($servilo,$username,$password);
-	    @mysql_select_db($database) or die( "Neebla elekti la datumbazon");
-	
-	    $ligilo_vk='';
-	    $ligilo_vk_mp3='';
-	
-	    $query_vk="SELECT Ligoj_al_diskoservo FROM vinilkosmo_tabelo WHERE Noms_albums_complets_et_Titres_par_piste_unitaire='$title' AND Artistoj='$artists' AND REF NOT LIKE 'VK%' AND Ligoj_al_diskoservo!='0'";
-	    $result=mysql_query($query_vk);
-	    if($result)
-	    {
-	      $ligilo_vk = mysql_fetch_row($result);
-	      $ligilo_vk = $ligilo_vk[0]; // mysql_fetch_row returns an array. we only want the Name so we just set it excluseively.
-	    }
-	
-	    $query_vk_mp3="SELECT Ligoj_al_la_elsxutejo FROM vinilkosmo_tabelo WHERE Noms_albums_complets_et_Titres_par_piste_unitaire='$title' AND Artistoj='$artists' AND REF NOT LIKE 'VK%' AND Ligoj_al_la_elsxutejo!='0'";
-	    $result=mysql_query($query_vk_mp3);
-	    if($result)
-	    {
-	      $ligilo_vk_mp3 = mysql_fetch_row($result);
-	      $ligilo_vk_mp3 = $ligilo_vk_mp3[0]; // mysql_fetch_row returns an array. we only want the Name so we just set it excluseively.
-	    }
-	
-	    mysql_close();
-	
-	    $protokoldosiero="nekonata.log";
-	    if (empty($ligilo_vk)) {
-	//       $ligilo_vk = 'http://vinilkosmo.com';
-	      $file = file_get_contents($protokoldosiero);
-	      if(!strpos($file, $query_vk)) {
-	        $myFile = $protokoldosiero;
-	        $fh = fopen($myFile, 'a') or die("can't open file");
-	        fwrite($fh, "== ligilo_vk ne trovata ==\n");
-	        fwrite($fh, $xml->track->artists . " - " . $xml->track->title . "\n");
-	        fwrite($fh, $query_vk . ";\n");
-	        fclose($fh);
-	      }
-	    }
-	    if (empty($ligilo_vk_mp3)) {
-	//       $ligilo_vk_mp3 = 'http://vinilkosmo-mp3.com';
-	      $file = file_get_contents($protokoldosiero);
-	      if(!strpos($file, $query_vk_mp3)) {
-	        $myFile = $protokoldosiero;
-	        $fh = fopen($myFile, 'a') or die("can't open file");
-	        fwrite($fh, "== ligilo_vk_mp3 ne trovata ==\n");
-	        fwrite($fh, $xml->track->artists . " - " . $xml->track->title . "\n");
-	        fwrite($fh, $query_vk_mp3 . ";\n");
-	        fclose($fh);
-	      }
-	    }
-	
-	    if ( !empty($ligilo_vk) && !empty($ligilo_vk_mp3) ) {
-	      echo '</br><a target="_blank" href="' . $ligilo_vk . '">Fizika albumo</a> - <a target="_blank" href="' . $ligilo_vk_mp3 . '">MP3</a>';
-         
-	    }
-	    if ( !empty($ligilo_vk) && empty($ligilo_vk_mp3) ) {
-	      echo '</br><a target="_blank" href="' . $ligilo_vk . '">Fizika albumo</a>';
-	    }
-	    if ( empty($ligilo_vk) && !empty($ligilo_vk_mp3) ) {
-	      echo '</br><a target="_blank" href="' . $ligilo_vk_mp3 . '">MP3</a>';
-	    }
-	
-	  }
-	}
-}
+else {//xml true
+
+  // preparu programtekston
+  $nuna_programo = '';
+  $ligu = false;
+  if ( !empty($xml->track->artists) ) {
+    $nuna_programo = $nuna_programo . cxapeligu($xml->track->artists);
+    $ligu = true;
+  }
+  if ( !empty($xml->track->title) ) {
+    if ( $ligu )
+    {
+      $nuna_programo = $nuna_programo . ' - ';
+    }
+    $nuna_programo = $nuna_programo . cxapeligu($xml->track->title);
+    $ligu = true;
+  }
+  echo wordwrap ( $nuna_programo,  $maxlen , "<br>" , true);
+
+  // preparu ligiltekston
+  mysql_connect($servilo,$username,$password);
+  @mysql_select_db($database) or die( "Neebla elekti la datumbazon");
+
+  $ligilo_vk='';
+  $ligilo_vk_mp3='';
+  $ligilo_muzikteksto='';
+  $ligilo_retpagxo='';
+  if ( !empty($xml->track->artists) && !empty($xml->track->title) ) { //muziko
+
+    $artists = trim(addslashes(malcxapeligu($xml->track->artists)));
+    $title = trim(addslashes(malcxapeligu($xml->track->title)));
+
+    $query_vk="SELECT Ligoj_al_diskoservo FROM vinilkosmo_tabelo WHERE Titolo='$title' AND Artistoj='$artists' AND REF NOT LIKE 'VK%' AND Ligoj_al_diskoservo!='0'";
+    $result=mysql_query($query_vk);
+    if($result)
+    {
+      $ligilo_vk = mysql_fetch_row($result);
+      $ligilo_vk = $ligilo_vk[0]; // mysql_fetch_row returns an array. we only want the Name so we just set it excluseively.
+    }
+
+    $query_vk_mp3="SELECT Ligoj_al_la_elsxutejo FROM vinilkosmo_tabelo WHERE Titolo='$title' AND Artistoj='$artists' AND REF NOT LIKE 'VK%' AND Ligoj_al_la_elsxutejo!='0'";
+    $result=mysql_query($query_vk_mp3);
+    if($result)
+    {
+      $ligilo_vk_mp3 = mysql_fetch_row($result);
+      $ligilo_vk_mp3 = $ligilo_vk_mp3[0]; // mysql_fetch_row returns an array. we only want the Name so we just set it excluseively.
+    }
+
+    $query_muzikteksto="SELECT Ligoj_al_muzikteksto FROM vinilkosmo_tabelo WHERE Titolo='$title' AND Artistoj='$artists' AND REF NOT LIKE 'VK%' AND Ligoj_al_muzikteksto!='0'";
+    $result=mysql_query($query_muzikteksto);
+    if($result)
+    {
+      $ligilo_muzikteksto = mysql_fetch_row($result);
+      $ligilo_muzikteksto = $ligilo_muzikteksto[0]; // mysql_fetch_row returns an array. we only want the Name so we just set it excluseively.
+    }
+
+    $query_retpagxo="SELECT Ligoj_al_retpagxo FROM vinilkosmo_tabelo WHERE Titolo='$title' AND Artistoj='$artists' AND REF NOT LIKE 'VK%' AND Ligoj_al_retpagxo!='0'";
+    $result=mysql_query($query_retpagxo);
+    if($result)
+    {
+      $ligilo_retpagxo = mysql_fetch_row($result);
+      $ligilo_retpagxo = $ligilo_retpagxo[0]; // mysql_fetch_row returns an array. we only want the Name so we just set it excluseively.
+    }
+
+  }
+  elseif ( empty($xml->track->artists) && !empty($xml->track->title) ) { // novajxoj, ktp
+
+    $title = trim(addslashes(malcxapeligu($xml->track->title)));
+
+    $query_vk="SELECT Ligoj_al_diskoservo FROM vinilkosmo_tabelo WHERE Titolo='$title' AND REF NOT LIKE 'VK%' AND Ligoj_al_diskoservo!='0'";
+    $result=mysql_query($query_vk);
+    if($result)
+    {
+      $ligilo_vk = mysql_fetch_row($result);
+      $ligilo_vk = $ligilo_vk[0]; // mysql_fetch_row returns an array. we only want the Name so we just set it excluseively.
+    }
+
+    $query_vk_mp3="SELECT Ligoj_al_la_elsxutejo FROM vinilkosmo_tabelo WHERE Titolo='$title' AND REF NOT LIKE 'VK%' AND Ligoj_al_la_elsxutejo!='0'";
+    $result=mysql_query($query_vk_mp3);
+    if($result)
+    {
+      $ligilo_vk_mp3 = mysql_fetch_row($result);
+      $ligilo_vk_mp3 = $ligilo_vk_mp3[0]; // mysql_fetch_row returns an array. we only want the Name so we just set it excluseively.
+    }
+
+    $query_muzikteksto="SELECT Ligoj_al_muzikteksto FROM vinilkosmo_tabelo WHERE Titolo='$title' AND REF NOT LIKE 'VK%' AND Ligoj_al_muzikteksto!='0'";
+    $result=mysql_query($query_muzikteksto);
+    if($result)
+    {
+      $ligilo_muzikteksto = mysql_fetch_row($result);
+      $ligilo_muzikteksto = $ligilo_muzikteksto[0]; // mysql_fetch_row returns an array. we only want the Name so we just set it excluseively.
+    }
+
+    $query_retpagxo="SELECT Ligoj_al_retpagxo FROM vinilkosmo_tabelo WHERE Titolo='$title' AND REF NOT LIKE 'VK%' AND Ligoj_al_retpagxo!='0'";
+    $result=mysql_query($query_retpagxo);
+    if($result)
+    {
+      $ligilo_retpagxo = mysql_fetch_row($result);
+      $ligilo_retpagxo = $ligilo_retpagxo[0]; // mysql_fetch_row returns an array. we only want the Name so we just set it excluseively.
+    }
+
+  }
+  mysql_close();
+
+  // protokolu netrovatajn ligilojn
+  $protokoldosiero="nekonata.log";
+  if (empty($ligilo_vk)) {
+    $file = file_get_contents($protokoldosiero);
+    if(!strpos($file, $query_vk)) {
+      $myFile = $protokoldosiero;
+      $fh = fopen($myFile, 'a') or die("can't open file");
+      fwrite($fh, "== ligilo_vk ne trovata ==\n");
+      fwrite($fh, $xml->track->artists . " - " . $xml->track->title . "\n");
+      fwrite($fh, $query_vk . ";\n");
+      fclose($fh);
+    }
+  }
+  if (empty($ligilo_vk_mp3)) {
+    $file = file_get_contents($protokoldosiero);
+    if(!strpos($file, $query_vk_mp3)) {
+      $myFile = $protokoldosiero;
+      $fh = fopen($myFile, 'a') or die("can't open file");
+      fwrite($fh, "== ligilo_vk_mp3 ne trovata ==\n");
+      fwrite($fh, $xml->track->artists . " - " . $xml->track->title . "\n");
+      fwrite($fh, $query_vk_mp3 . ";\n");
+      fclose($fh);
+    }
+  }
+  if (empty($ligilo_muzikteksto)) {
+    $file = file_get_contents($protokoldosiero);
+    if(!strpos($file, $query_muzikteksto)) {
+      $myFile = $protokoldosiero;
+      $fh = fopen($myFile, 'a') or die("can't open file");
+      fwrite($fh, "== ligilo_muzikteksto ne trovata ==\n");
+      fwrite($fh, $xml->track->artists . " - " . $xml->track->title . "\n");
+      fwrite($fh, $query_muzikteksto . ";\n");
+      fclose($fh);
+    }
+  }
+  if (empty($ligilo_retpagxo)) {
+    $file = file_get_contents($protokoldosiero);
+    if(!strpos($file, $query_retpagxo)) {
+      $myFile = $protokoldosiero;
+      $fh = fopen($myFile, 'a') or die("can't open file");
+      fwrite($fh, "== ligilo_retpagxo ne trovata ==\n");
+      fwrite($fh, $xml->track->artists . " - " . $xml->track->title . "\n");
+      fwrite($fh, $query_retpagxo . ";\n");
+      fclose($fh);
+    }
+  }
+
+  // fine kreu por montru la ligiltekston
+  $ligiloj_trovitaj = false;
+  $ligilteksto = '</br>';
+  if ( !empty($ligilo_vk) )
+  {
+    if ( $ligiloj_trovitaj )
+    {
+      $ligilteksto = $ligilteksto . ' - ';
+    }
+    $ligilteksto = $ligilteksto . '<a target="_blank" href="' . $ligilo_vk . '">Fizika albumo</a>';
+    $ligiloj_trovitaj = true; 
+  }
+  if ( !empty($ligilo_vk_mp3) )
+  {
+    if ( $ligiloj_trovitaj )
+    {
+      $ligilteksto = $ligilteksto . ' - ';
+    }
+    $ligilteksto = $ligilteksto . '<a target="_blank" href="' . $ligilo_vk_mp3 . '">MP3</a>'; 
+    $ligiloj_trovitaj = true; 
+  }
+  if ( !empty($ligilo_muzikteksto) )
+  {
+    if ( $ligiloj_trovitaj )
+    {
+      $ligilteksto = $ligilteksto . ' - ';
+    }
+    $ligilteksto = $ligilteksto . '<a target="_blank" href="' . $ligilo_muzikteksto . '">Muzikteksto</a>'; 
+    $ligiloj_trovitaj = true; 
+  }
+  if ( !empty($ligilo_retpagxo) )
+  {
+    if ( $ligiloj_trovitaj )
+    {
+      $ligilteksto = $ligilteksto . ' - ';
+    }
+    $ligilteksto = $ligilteksto . '<a target="_blank" href="' . $ligilo_retpagxo . '">Retpaĝo</a>'; 
+    $ligiloj_trovitaj = true; 
+  }
+  if ( $ligiloj_trovitaj )
+  {
+    echo $ligilteksto;
+  }
+
+}//xml true
 
 ?>
