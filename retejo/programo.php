c32259eb58f7a8c05b38f0b626f18ea4fbf71758
Gérald Gounot <gerald@gounot.eu>
2011-12-30 12:45:45 +0100

    Aldonu paĝon, kiu montras nur la programo
    Por Android

---
+++ retejo/programo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Programo de Muzaiko</title>
+<link rel="stylesheet" type="text/css" href="images/style.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="images/style_calendar.css" media="screen" />
+<link rel="icon" type="image/png" href="images/favicon.png" />
+</head>
+
+<?php
+include('inc/programo_html.php');
+?>
