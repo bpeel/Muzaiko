88482292b4c53f76150805b29dc613232995444e
Gérald Gounot <gerald@gounot.eu>
2012-01-11 15:11:32 +0100

    Aldonu JS-funkcion por ĉapeligi tekstkampojn

---
+++ retejo/cxapeligado.js
@@ -0,0 +1,23 @@
+function cxapeligado(event) {
+    this.value = this.value.replace(/c[xX]/g, 'ĉ');
+    this.value = this.value.replace(/C[xX]/g, 'Ĉ');
+    this.value = this.value.replace(/g[xX]/g, 'ĝ');
+    this.value = this.value.replace(/G[xX]/g, 'Ĝ');
+    this.value = this.value.replace(/h[xX]/g, 'ĥ');
+    this.value = this.value.replace(/H[xX]/g, 'Ĥ');
+    this.value = this.value.replace(/j[xX]/g, 'ĵ');
+    this.value = this.value.replace(/J[xX]/g, 'Ĵ');
+    this.value = this.value.replace(/s[xX]/g, 'ŝ');
+    this.value = this.value.replace(/S[xX]/g, 'Ŝ');
+    this.value = this.value.replace(/u[xX]/g, 'ŭ');
+    this.value = this.value.replace(/U[xX]/g, 'Ŭ');
+}
+
+window.onload = function() {
+    var inputs = Array.prototype.slice.call(document.getElementsByTagName("input"));
+    var nodes = inputs.concat(Array.prototype.slice.call(document.getElementsByTagName("textarea")));
+    for(var i = 0; i < nodes.length; i++) {
+        nodes[i].addEventListener('keyup', cxapeligado, false);
+    }
+}
+
