023171d837bb87d2ef3f657cec244d1e5fc99753
Gérald Gounot <gerald@gounot.eu>
2012-01-12 10:56:42 +0100

    Korektu SQL-informmendon

--- retejo/programo/index.php
+++ retejo/programo/index.php
@@ -378,7 +378,7 @@ function konservu_sondosierojn($programero_id, &$success, &$errors)
 
 <?php
 
-$query = "SELECT id, description FROM programero, elsendo WHERE id = programero_id GROUP BY id ORDER BY date_begin ASC";
+$query = "SELECT id, description FROM programero, elsendo WHERE id = programero_id GROUP BY id ORDER BY MAX(date_begin) ASC";
 $result = mysql_query($query);
 while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
 	printf('<tr><td>%s</td><td class="date_column"><ul>', format_programero(htmlspecialchars(stripslashes($row[1]))));
