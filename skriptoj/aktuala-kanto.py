056dc93ea0f1afdf4822392855d417a98ed7e3c0
Neil Roberts <bpeeluk@yahoo.co.uk>
2012-02-21 02:13:39 +0000

    aktuala-kanto: Konvertu la petojn al klasoj
    Tiel la klasoj ĉiuj povas havi malsaman paŭzon inter ĝisdatigoj.

--- skriptoj/aktuala-kanto.py
+++ skriptoj/aktuala-kanto.py
@@ -7,64 +7,87 @@ import cgi
 import os
 import pkagordoj
 
-# Kiom da sekundoj inter ĉiu ĝisdatigo
-SEKUNDOJ_INTER_GXISDATIGO = 15
+RADIOUID = "14694a7d-9023-4db1-86b4-d85d96cba181"
 
-URL_POR_NOMBRO_DE_AUXSKULTANTOJ = \
-    "http://api.radionomy.com/currentaudience.cfm?" \
-    "radiouid=14694a7d-9023-4db1-86b4-d85d96cba181"
+class Peto:
+    def __init__(self):
+        self.pasinta_gxisdatigo = 0
+        self.sekva_gxisdatigo = 0
 
-def transformu_nombron_de_auxskultantoj(datumo):
-    return "Nombro de aŭskultantoj " \
-        "(laŭ Radionomy, eble ne tute preciza): " + \
-        cgi.escape(datumo)
+    def anstatauxigu_dosieron(self, datumo):
+        # Unue eligu la datumon al portempa dosiero
+        dosiero = open(self.DOSIERO + ".tmp", "w")
+        dosiero.write(datumo)
+        dosiero.close()
 
-def anstatauxigu_dosieron(dosiernomo, datumo):
-    # Unue eligu la datumon al portempa dosiero
-    dosiero = open(dosiernomo + ".tmp", "w")
-    dosiero.write(datumo)
-    dosiero.close()
+        # Renomu la originalan dosieron al la nova kiel atoma ago por ke
+        # la dosiero neniam estos duone plenumita
+        os.rename(self.DOSIERO + ".tmp", self.DOSIERO)
 
-    # Renomu la originalan dosieron al la nova kiel atoma ago por ke
-    # la dosiero neniam estos duone plenumita
-    os.rename(dosiernomo + ".tmp", dosiernomo)
-
-def gxisdatigu(url, transformo, dosiernomo):
-    # Prenu la datumon el la URL kaj silente ignoru erarojn
-    try:
-        dosiero = urllib.urlopen(url)
+    def gxisdatigu(self):
+        # Prenu la datumon el la URL kaj silente ignoru erarojn
         try:
-            datumo = dosiero.read()
-        finally:
-            dosiero.close()
-    except IOError:
-        pass
-    else:
-        # Transformu la datumon
-        datumo = transformo(datumo)
-
-        try:
-            anstatauxigu_dosieron(dosiernomo, datumo)
+            dosiero = urllib.urlopen(self.URL)
+            try:
+                datumo = dosiero.read()
+            finally:
+                dosiero.close()
         except IOError:
             pass
+        else:
+            # Transformu la datumon
+            datumo = self.transformu(datumo)
+
+            try:
+                Peto.anstatauxigu_dosieron(self, datumo)
+            except IOError:
+                pass
+        finally:
+            self.pasinta_gxisdatigo = time.time()
+            self.sekva_gxisdatigo = self.pasinta_gxisdatigo + self.pauxzo
+
+class NombroDeAuxskultantoj(Peto):
+    URL = \
+        "http://api.radionomy.com/currentaudience.cfm?" \
+        "radiouid=" + RADIOUID
+
+    DOSIERO = \
+        pkagordoj.get("loko_de_ajax") + \
+        "/nombro_de_auxskultantoj.html"
+
+    def __init__(self):
+        Peto.__init__(self)
+
+        # Atendu la sekvan ĝisdatigon dum 15 sekundoj
+        self.pauxzo = 15
+
+    def transformu(self, datumo):
+        return "Nombro de aŭskultantoj " \
+            "(laŭ Radionomy, eble ne tute preciza): " + \
+            cgi.escape(datumo)
 
 def cxefiteracio():
-    while True:
-        pasinta_gxisdatigo = time.time()
+    nun = 0
+    petoj = [ NombroDeAuxskultantoj() ]
 
-        gxisdatigu(URL_POR_NOMBRO_DE_AUXSKULTANTOJ,
-                   transformu_nombron_de_auxskultantoj,
-                   pkagordoj.get("loko_de_ajax") +
-                   "/nombro_de_auxskultantoj.html")
+    while True:
+        # Ĝisdatigu ĉiujn petojn kiuj jam pretas
+        for peto in petoj:
+            if (nun >= peto.sekva_gxisdatigo or
+                nun < peto.pasinta_gxisdatigo):
+                peto.gxisdatigu()
 
         # Dormu ĝis la sekva ĝisdatigo
         while True:
+            sekva_gxisdatigo = min([peto.sekva_gxisdatigo for peto in petoj])
+            pasinta_gxisdatigo = max([peto.pasinta_gxisdatigo
+                                      for peto in petoj])
+
             nun = time.time()
-            if (nun >= pasinta_gxisdatigo + SEKUNDOJ_INTER_GXISDATIGO or
-                nun < pasinta_gxisdatigo):
+
+            if (nun >= sekva_gxisdatigo or nun < pasinta_gxisdatigo):
                 break
 
-            time.sleep(pasinta_gxisdatigo +
-                       SEKUNDOJ_INTER_GXISDATIGO - nun + 0.05)
+            time.sleep(sekva_gxisdatigo - nun + 0.05)
 
 cxefiteracio()
