031eadf78404ba0c1525cd816f6ce897ed7233c4
Neil Roberts <bpeeluk@yahoo.co.uk>
2012-12-04 20:18:27 +0000

    Aldonu komencon de paĝo por la detaloj pri programero
    Ĉi tiu aldonas kodon kiu povas inkluziviĝi en paĝo de la CMS por
    montri detalojn de unu programo. Tiu inkluzivas aŭskultilon por ludigi
    la sondosieron kaj montri la subtitolojn.

---
+++ programdetaloj/programdetaloj.php
@@ -0,0 +1,137 @@
+<?php
+
+require_once ("inc/inc.php");
+require_once ("inc/programo.php");
+
+function montru_kampon ($linio, $kampo, $titolo, $enhavo)
+{
+  if ($enhavo || $linio[$kampo])
+    {
+      echo ("<tr><td class=\"kampo\">$titolo</td><td>");
+
+      if ($enhavo)
+        echo ($enhavo);
+      else
+        echo (format_programero (htmlspecialchars ($linio[$kampo])));
+
+      echo ("</td></tr>\n");
+    }
+}
+
+function kuiri_tekston ($teksto)
+{
+  return preg_replace ('/[0-9]+\n' .
+                       '[0-9]+:[0-9]+:[0-9]+,[0-9]+ --> ' .
+                       '[0-9]+:[0-9]+:[0-9]+,[0-9]+\n/',
+                       "",
+                       $teksto);
+}
+
+function sonfonto ($id, $tipo)
+{
+  return ("<source src=\"../sondosieroj/programero" .
+          $id . "." . $tipo . "\" " .
+          "type=\"audio/" . $tipo . "\" />\n");
+}
+
+function format_permesilo ($nomo, $bildo, $url = null)
+{
+  if ($nomo)
+    {
+      if ($bildo)
+        $permesilo =
+          "<img src=\"images/permesiloj/" .
+          htmlentities ($bildo) .
+          "\" alt=\"" .
+          htmlentities ($nomo) . "\" title=\"" .
+          htmlentities ($nomo) ."\" />";
+      else
+        $permesilo = htmlentities ($nomo);
+
+      if ($url)
+        $permesilo =
+          "<a href=\"" . htmlentities ($url) . "\">" .
+          $permesilo .
+          "</a>";
+    }
+  else
+    $permesilo = "";
+
+  return $permesilo;
+}
+
+konektu_al_programo ();
+
+$rez = mysql_query ("select `programero`.`id`, " .
+                    "`programero`.`titolo`, " .
+                    "`programero`.`skizo`, " .
+                    "`permesilo`.`nomo` as `permesilo_nomo`, " .
+                    "`permesilo`.`bildo` as `permesilo_bildo`, " .
+                    "`permesilo`.`url` as `permesilo_url`, " .
+                    "`programero`.`teksto`, " .
+                    "count(`sondosiero`.`nomo`) as `sondosieroj` " .
+                    "from `programero` " .
+                    "left join `permesilo` " .
+                    "on `permesilo`.`id` = `programero`.`permesilo_id` " .
+                    "left join `sondosiero` " .
+                    "on `sondosiero`.`programero` = `programero`.`id` " .
+                    "where `programero`.`id` = '" .
+                    mysql_real_escape_string ($_REQUEST["id"]) ."' " .
+                    "group by `programero`.`id`");
+
+while ($linio = mysql_fetch_array ($rez, MYSQL_ASSOC))
+  {
+    if ($linio["titolo"])
+      echo ("<h1>" . htmlspecialchars ($linio["titolo"]) . "</h1>\n");
+    else
+      echo ("<h1>Programdetaloj</h1>\n");
+
+    echo ("<table id=\"detaltabelo\">\n");
+
+    montru_kampon ($linio, "skizo", "Skizo");
+
+    $kruda_teksto = $linio["teksto"];
+    $kuirita_teksto = kuiri_tekston ($kruda_teksto);
+
+    if ($linio["permesilo_nomo"])
+      montru_kampon ($linio, "permesilo_nomo", "Permesilo",
+                     format_permesilo ($linio["permesilo_nomo"],
+                                       $linio["permesilo_bildo"],
+                                       $linio["permesilo_url"]));
+
+    if ($linio["sondosieroj"] > 0)
+      {
+        $auxskultilo =
+          "<audio controls=\"controls\" id=\"auxskultilo\">\n" .
+          sonfonto ($linio["id"], "mp3") .
+          sonfonto ($linio["id"], "ogg") .
+          "</audio>\n";
+
+        /* Se la teksto havas tempojn ni ankaŭ lasos spacon por montri
+         * la titolojn dum ĝi ludiĝas */
+        if ($kruda_teksto != $kuirita_teksto)
+          {
+            $auxskultilo .=
+              "<div id=\"titoloj\" style=\"" .
+              "font-size:large;padding:1em;" .
+              "background-color:rgb(200,200,255);" .
+              "height:4em;text-align:center\"" .
+              "></div>\n" .
+              "<script type=\"text/javascript\" src=\"subtitoloj.js\">\n" .
+              "</script>\n" .
+              "<div id=\"subtitoldatumo\" style=\"display:none\">" .
+              htmlspecialchars ($kruda_teksto) .
+              "</div>\n";
+          }
+
+        montru_kampon ($linio, "sondosieroj", "Aŭskultu", $auxskultilo);
+      }
+
+    montru_kampon ($linio, "teksto", "Teksto",
+                   nl2br (htmlspecialchars ($kuirita_teksto)));
+
+    echo ("</table>\n");
+  }
+
+mysql_free_result ($rez);
+?>
---
+++ programdetaloj/subtitoloj.js
@@ -0,0 +1,133 @@
+(function ()
+ {
+   var aktuala_titolo = null;
+   var cxiuj_titoloj;
+   var auxskultilo;
+   var titoloj_div;
+
+   function akiru_tempon (a, b, c, d)
+   {
+     return (parseInt (d, 10) +
+             parseInt (c, 10) * 1000 +
+             parseInt (b, 10) * 60000 +
+             parseInt (a, 10) * 60000000);
+   }
+
+   function aldonu_titolon (rez, lasta_fino, komenco, teksto)
+   {
+     var linifino;
+     var elemoj;
+     var lasta_loko = 0;
+
+     /* Se estas paŭzo inter la lasta titolo kaj la nova, aldonu novan
+      * blankan titolon */
+     if (rez.length > 0 && lasta_fino < komenco)
+       rez.push ([lasta_fino]);
+
+     elemoj = [komenco];
+
+     while ((linifino = teksto.indexOf ("\n", lasta_loko)) >= 0)
+     {
+       if (linifino > lasta_loko)
+       {
+         var parto = teksto.substring (lasta_loko, linifino);
+         elemoj.push (document.createTextNode (parto));
+       }
+       elemoj.push (document.createElement ("br"));
+       lasta_loko = linifino + 1;
+     }
+     if (lasta_loko < teksto.length)
+       elemoj.push (document.createTextNode (teksto.substring (lasta_loko)));
+
+     rez.push (elemoj);
+   }
+
+   function akiru_titolojn (teksto)
+   {
+     var re = /(?:^|\n)[0-9]+\n([0-9]{2}):([0-9]{2}):([0-9]{2}),([0-9]{3}) --> ([0-9]{2}):([0-9]{2}):([0-9]{2}),([0-9]{3})\n/;
+     var rez = [];
+     var lasta_komenco = 0;
+     var lasta_fino = 0;
+     var antauxlasta_fino = 0;
+
+     while (true)
+     {
+       var trovajxo = re.exec (teksto);
+
+       if (!trovajxo)
+         break;
+
+       var titolo = teksto.substring (0, trovajxo.index);
+       if (/[^\s]/.exec (titolo))
+         aldonu_titolon (rez, antauxlasta_fino, lasta_komenco, titolo);
+
+       antauxlasta_fino = lasta_fino;
+       lasta_komenco = akiru_tempon (trovajxo[1],
+                                   trovajxo[2],
+                                   trovajxo[3],
+                                   trovajxo[4]);
+       lasta_fino = akiru_tempon (trovajxo[5],
+                                  trovajxo[6],
+                                  trovajxo[7],
+                                  trovajxo[8]);
+
+       teksto = teksto.substring (trovajxo.index + trovajxo[0].length);
+     }
+
+     if (/[^\s]/.exec (teksto))
+     {
+       aldonu_titolon (rez, antauxlasta_fino, lasta_komenco, teksto);
+       rez.push ([lasta_fino]);
+     }
+
+     return rez;
+   }
+
+   function time_update_cb ()
+   {
+     var aktuala_tempo = auxskultilo.currentTime * 1000.0;
+     var min = 0, maks = cxiuj_titoloj.length;
+
+     /* Duuma tranĉo por trovi la aktualan titolon */
+     while (maks > min)
+     {
+       var mez = Math.floor ((maks + min) / 2);
+       var mez_tempo = cxiuj_titoloj[mez][0];
+
+       if (mez_tempo < aktuala_tempo)
+         min = mez + 1;
+       else
+         maks = mez;
+     }
+
+     var nova_titolo = cxiuj_titoloj[min > 0 ? min - 1 : 0];
+
+     if (nova_titolo != aktuala_titolo)
+     {
+       var i;
+
+       titoloj_div.innerHTML = "";
+       for (i = 1; i < nova_titolo.length; i++)
+         titoloj_div.appendChild (nova_titolo[i]);
+
+       aktuala_titolo = nova_titolo;
+     }
+   }
+
+   function komencu ()
+   {
+     var datumelem = document.getElementById ("subtitoldatumo");
+
+     cxiuj_titoloj = akiru_titolojn (datumelem.textContent);
+
+     if (cxiuj_titoloj.length > 1)
+     {
+       titoloj_div = document.getElementById ("titoloj");
+       auxskultilo = document.getElementById ("auxskultilo");
+       auxskultilo.addEventListener ("timeupdate", time_update_cb, false);
+     }
+   }
+
+   if (window.addEventListener)
+     window.addEventListener ("load", komencu, false);
+ }) ();
